b92bab12c0276a56286fda88e9fdc64d788ed261
Federico Jaureguialzo <fedejaure@gmail.com>
2024-07-07 21:11:15 +0200

    Add tests

--- src/flintpy/utils.py
+++ src/flintpy/utils.py
@@ -1,10 +1,8 @@
 """Utils functions for flintpy."""
 
-from typing import Tuple
-
 import numpy as np
 
 
-def logarithm_t_range(t_range: Tuple[float, float], kernel_dim: int) -> np.ndarray:
+def logarithm_t_range(t_range: tuple[float, float], kernel_dim: int) -> np.ndarray:
     """Generates a logarithmic time range."""
     return np.logspace(np.log10(t_range[0]), np.log10(t_range[1]), num=kernel_dim)
